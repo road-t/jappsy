f770589d3ce8410af73f53b6e7458a5e59090291
Jappsy Master <jappsydev@gmail.com>
2016-08-02 02:32:09 +0300

    Android EConvert EInvalidPointer EOutOfRange

---
+++ src/platform/Android/app/src/main/java/com/jappsy/exceptions/EConvert.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2016 The Jappsy Open Source Project (http://jappsy.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jappsy.exceptions;
+
+public class EConvert extends EGlobal {
+
+    private static final long serialVersionUID = -5503258691856629653L;
+    private static final String eConvert = "Convert";
+
+    public EConvert() {
+        super(eConvert);
+    }
+
+    public EConvert(String detailMessage) {
+        super(detailMessage);
+    }
+
+    public EConvert(Throwable throwable) {
+        super(eConvert, throwable);
+    }
+
+    public EConvert(String detailMessage, Throwable throwable) {
+        super(detailMessage, throwable);
+    }
+
+}

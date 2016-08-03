366d3816f4eb6945841f423e0f45e355f041a30e
Jappsy Master <jappsydev@gmail.com>
2016-08-03 08:04:17 +0300

    starting hardwork for iOS - the base core project

---
+++ src/platform/iOS/modCore/modCoreTests/modCoreTests.mm
@@ -0,0 +1,39 @@
+//
+//  modCoreTests.m
+//  modCoreTests
+//
+//  Created by VipDev on 02.08.16.
+//  Copyright © 2016 Jappsy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface modCoreTests : XCTestCase
+
+@end
+
+@implementation modCoreTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
